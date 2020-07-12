5846326401e58d627ef2271463e81f3784bd1596
Thiago Jorge <thiago.v.jorge@hotmail.com>
2020-07-12 15:58:50 -0300

    Bckp

--- criptografia/index.py
+++ criptografia/index.py
@@ -1,8 +1,13 @@
-# Na primeira passada, somente caracteres que sejam letras minúsculas e maiúsculas devem ser deslocadas 3 posições para a direita, segundo a tabela ASCII: letra 'a' deve virar letra 'd', letra 'y' deve virar caractere '|' e assim sucessivamente. 
+# Na primeira passada, somente caracteres que sejam letras minúsculas e maiúsculas 
+# 
+# devem ser deslocadas 3 posições para a direita, segundo a tabela ASCII: letra 'a' deve virar letra 'd', 
+# letra 'y' deve virar caractere '|' e assim sucessivamente. 
 
 # Na segunda passada, a linha deverá ser invertida. 
 
-# Na terceira e última passada, todo e qualquer caractere a partir da metade em diante (truncada) devem ser deslocados uma posição para a esquerda na tabela ASCII. Neste caso, 'b' vira 'a' e 'a' vira '`'.
+# Na terceira e última passada, todo e qualquer caractere a partir da metade em diante (truncada) 
+# devem ser deslocados uma posição para a esquerda na tabela ASCII. Neste caso, 'b' vira 'a' e 'a' vira '`'.
+
 
 # text = list(input("Insira o texto a ser criptografado: "))
 
@@ -14,7 +19,10 @@ messages = []
 for x in range(1, int(input("Insira a quantidade de textos a serem criptografados: "))+1):
   messages.append(input("Mensagem: "))
 
+for message in messages:
+  for char in message:
+    if(char.isalpha()):
+      print(char)
 
 
 #chr(ord(texto[2]) + 1)
-
